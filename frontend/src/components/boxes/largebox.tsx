6dfc6d347b4b06380a77eec905b9ddfa4538287d
Tomas Gonzalez <tomasgonzalezuptp@gmail.com>
2026-03-04 10:44:35 -0300

    feature: improve dashboard and finish page

--- frontend/src/components/boxes/largebox.tsx
+++ frontend/src/components/boxes/largebox.tsx
@@ -11,7 +11,7 @@ export default function LargeBox({ title, subtitle }: LargeBoxProps) {
                 <h3 className="text-xl font-bold text-slate-700">{title}</h3>
                 <p className="mt-2text-slate-500 text-sm font-medium mt-2">{subtitle}</p>
             </div>
-            <button className="bg-green-500 text-white px-4 py-2 rounded-lg mt-4">Ver</button>
+
 
         </div>
     )
